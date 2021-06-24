a17dc0772843df6a8b29dc190bc778d77e6a092a
Nazmul-hasan69 <hmdnazmul69@gmail.com>
2021-06-24 15:59:36 +0600

    modifie slider section

--- js/app.js
+++ js/app.js
@@ -1,3 +1,6 @@
 $(function () {
-    $('.slider_container').slick();
+    $('.slider_container').slick({
+        prevArrow:'<i class="slider_arrow slider_arrow_left fas fa-arrow-circle-left"></i>',
+        nextArrow:'<i class="slider_arrow slider_arrow_right fas fa-arrow-circle-right"></i>'
+    });
 })
